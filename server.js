a74c730311f183107b2e6aea5bf825a518010bcf
Jean-Cedrick Lepin <jc_lepin@yahoo.fr>
2021-04-16 16:56:41 +0200

    add CORS

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const jwtMiddelware = require("express-jwt");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+const hash = async (password) => {
+  return await bcrypt.hash(password, saltRounds);
+};
+
+const compare = async (password, hash) => {
+  return await bcrypt.compare(password, hash);
+};
+const jwt = require("jsonwebtoken");
 // const Sequelize = require("sequelize");
 const knex = require("knex")({
   client: "sqlite3",
@@ -10,8 +22,16 @@ const knex = require("knex")({
   useNullAsDefault: true,
 });
 
+const SECRET = process.env.JWT_SECRET;
+
+const corsOptions = {
+  origin: process.env.FRONT_URL,
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
 app = express();
 app.use(express.json());
+app.use(cors(corsOptions));
 
 // const sequelize = new Sequelize("sqlite:database.db");
 
@@ -20,7 +40,7 @@ app.use(
     secret: process.env.JWT_SECRET,
     algorithms: ["HS256"],
   }).unless({
-    path: ["/posts"],
+    path: ["/posts", "/users", "/login"],
   })
 );
 
@@ -28,18 +48,47 @@ app.use((err, req, rep, next) => {
   err ? rep.status(err.status).json({ error: err.message }) : next();
 });
 
-const posts = [
-  { titre: "1 post" },
-  { titre: "2 post" },
-  { titre: "3 post" },
-  { titre: "4 post" },
-  { titre: "5 post" },
-  { titre: "6 post" },
-];
+app.post("/login", async (req, rep) => {
+  const { login, password } = req.body;
+
+  let utilisateur = await knex("user")
+    .where("login", "like", login)
+    .select(`*`);
+  utilisateur = utilisateur[0];
+  if (!utilisateur) {
+    rep.json({ erreur: "non autorisé" });
+  } else {
+    compare(password, utilisateur.hash).then((same) => {
+      if (same) {
+        const token = jwt.sign(
+          {
+            login,
+            role: utilisateur.role,
+          },
+          SECRET,
+          {
+            expiresIn: "1h",
+          }
+        );
+        rep.json({ token });
+      } else {
+        rep.status(403).json({ erreur: "non autorisé" });
+      }
+    });
+  }
+});
 
 app.get("/posts", async (req, res) => {
-  const posts = await knex("publication").select("*");
+  //   const posts = await knex("publication").select("*");
+  const posts = await knex("user")
+    .join("publication", "user.id", "publication.user_id")
+    .select("*");
   res.json(posts);
+});
+app.get("/users", async (req, res) => {
+  const users = await knex("user").select("*");
+  console.log("🚀 ~ file: server.js ~ line 40 ~ app.get ~ users", users);
+  res.json(users);
 });
 
 // sequelize
